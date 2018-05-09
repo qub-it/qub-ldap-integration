53a911492a685ab0f7a85085cc9e491a5ca4cd75
Luís Egídio <luis.egidio@gmail.com>
2018-05-09 12:09:44 +0100

    NEW-DEV-COMMIT - Change versions for rebase - Fix several bugs after rebase

--- src/main/java/com/qubit/solution/fenixedu/integration/ldap/service/ULisboaProfileResource.java
+++ src/main/java/com/qubit/solution/fenixedu/integration/ldap/service/ULisboaProfileResource.java
@@ -1,6 +1,6 @@
 /**
- * This file was created by Quorum Born IT <http://www.qub-it.com/> and its 
- * copyright terms are bind to the legal agreement regulating the FenixEdu@ULisboa 
+ * This file was created by Quorum Born IT <http://www.qub-it.com/> and its
+ * copyright terms are bind to the legal agreement regulating the FenixEdu@ULisboa
  * software development project between Quorum Born IT and Serviços Partilhados da
  * Universidade de Lisboa:
  *  - Copyright © 2015 Quorum Born IT (until any Go-Live phase)
@@ -8,7 +8,7 @@
  *
  * Contributors: paulo.abrantes@qub-it.com
  *
- * 
+ *
  * This file is part of FenixEdu fenixedu-ulisboa-ldapIntegration.
  *
  * FenixEdu fenixedu-ulisboa-ldapIntegration is free software: you can redistribute it and/or modify
@@ -27,6 +27,7 @@
 package com.qubit.solution.fenixedu.integration.ldap.service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -35,18 +36,19 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.lang.StringUtils;
+import org.fenixedu.bennu.core.api.ProfileResource;
+import org.fenixedu.bennu.core.api.json.AuthenticatedUserViewer;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.domain.UsernameHack;
 import org.fenixedu.bennu.core.domain.exceptions.AuthorizationException;
 import org.fenixedu.bennu.core.groups.Group;
-import org.fenixedu.bennu.core.json.adapters.AuthenticatedUserViewer;
-import org.fenixedu.bennu.core.rest.ProfileResource;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.ulisboa.specifications.ULisboaConfiguration;
 import org.fenixedu.ulisboa.specifications.ULisboaConfiguration.ConfigurationProperties;
 
+import com.google.gson.JsonElement;
 import com.qubit.solution.fenixedu.integration.ldap.domain.configuration.LdapServerIntegrationConfiguration;
 import com.qubit.terra.ldapclient.LdapClient;
 import com.qubit.terra.ldapclient.QueryReply;
@@ -63,7 +65,7 @@ public class ULisboaProfileResource extends ProfileResource {
     HttpServletRequest request;
 
     // In the JSPs this url will be linked through a login.html put in the
-    // main theme of the ULisboa project. Which will basically only changes  
+    // main theme of the ULisboa project. Which will basically only changes
     // the login URL.
     //
     // 22 June 2015 - Paulo Abrantes
@@ -71,15 +73,16 @@ public class ULisboaProfileResource extends ProfileResource {
     @POST
     @Path("loginExtension")
     @Produces(MediaType.APPLICATION_JSON)
-    public String login(@FormParam("username") String username, @FormParam("password") String password) {
+    public JsonElement login(@Context final HttpServletRequest request, @Context final HttpServletResponse response,
+            @FormParam("username") final String username, @FormParam("password") final String password) {
         LdapServerIntegrationConfiguration defaultLdapServer = Bennu.getInstance().getDefaultLdapServerIntegrationConfiguration();
         ConfigurationProperties configuration = ULisboaConfiguration.getConfiguration();
 
         if (isValidQualityAuthentication(username, password, configuration)) {
-            Authenticate.login(request.getSession(true), username);
+            Authenticate.login(request, response, User.findByUsername(username), "TODO: CHANGE ME");
             return view(null, Void.class, AuthenticatedUserViewer.class);
         } else if (defaultLdapServer == null || Boolean.TRUE == CoreConfiguration.getConfiguration().developmentMode()) {
-            return super.login(username, password);
+            return super.login(request, response, username, password);
         } else {
             LdapClient client = defaultLdapServer.getClient();
             String ldapUsername = username.replace("@campus.ul.pt", "").toLowerCase().trim();
@@ -114,7 +117,7 @@ public class ULisboaProfileResource extends ProfileResource {
                         }
                     }
                 }
-                Authenticate.login(request.getSession(true), ldapUsername);
+                Authenticate.login(request, response, User.findByUsername(ldapUsername), "TODO: CHANGE ME");
                 return view(null, Void.class, AuthenticatedUserViewer.class);
             }
         }
@@ -122,23 +125,24 @@ public class ULisboaProfileResource extends ProfileResource {
         throw AuthorizationException.authenticationFailed();
     }
 
-    private boolean isValidQualityAuthentication(String username, String password, ConfigurationProperties configuration) {
-        if(!Boolean.TRUE.equals(configuration.isQualityMode())){
+    private boolean isValidQualityAuthentication(final String username, final String password,
+            final ConfigurationProperties configuration) {
+        if (!Boolean.TRUE.equals(configuration.isQualityMode())) {
             return false;
         }
         User user = User.findByUsername(username);
         boolean isManager = Group.parse("#managers").isMember(user);
-        
+
         boolean isValidMasterPasswordLogin = !StringUtils.isEmpty(password) && password.equals(configuration.getMasterPassword());
-        
-        boolean isValidLightMasterPasswordLogin = !StringUtils.isEmpty(password) && password.equals(configuration.getLightMasterPassword()) &&
-                 !isManager;
-        
+
+        boolean isValidLightMasterPasswordLogin =
+                !StringUtils.isEmpty(password) && password.equals(configuration.getLightMasterPassword()) && !isManager;
+
         return isValidMasterPasswordLogin || isValidLightMasterPasswordLogin;
     }
 
     @Atomic
-    private void usernameAlign(String oldUsername, String newUsername) {
+    private void usernameAlign(final String oldUsername, final String newUsername) {
         UsernameHack.changeUsername(oldUsername, newUsername);
     }
 }
